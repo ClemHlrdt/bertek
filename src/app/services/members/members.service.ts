788bb2e0e33ba1fb3b795724ca7793c1a0cb1586
Clément Hélardot <clement.helardot@gmail.com>
2020-10-18 14:12:52 +0200

    Create a new Card instead of MemberCard

--- src/app/services/members/members.service.ts
+++ src/app/services/members/members.service.ts
@@ -2,8 +2,9 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { map, pluck } from 'rxjs/operators';
 import { Block } from 'src/app/shared/model/block.model';
-import { MemberCard } from 'src/app/shared/model/member-card.model';
+import { Card } from 'src/app/shared/model/card.model';
 import { ResponseModel } from 'src/app/shared/model/response.model';
+import { Image } from 'src/app/shared/model/image.model';
 
 @Injectable({
   providedIn: 'root',
@@ -23,7 +24,13 @@ export class MembersService {
           const title = attributes.title;
           const members = Object.values(attributes.memberCards).map(
             (card: any) => {
-              return new MemberCard(card.imageUrl, card.block);
+              return new Card(
+                new Image(card.imageUrl),
+                card.block.title,
+                card.block.description,
+                card.block.link,
+                card.block.text
+              );
             }
           );
           return new Block(title, members);
