234de4ea12beead6b045ffdf246bfa3fcc99eba3
Clément Hélardot <clement.helardot@gmail.com>
2020-10-18 14:11:30 +0200

    Removed srcSet logic from card component

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,6 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { Image } from 'src/app/shared/model/image.model';
-import { MemberCard } from 'src/app/shared/model/member-card.model';
+import { Card } from 'src/app/shared/model/card.model';
 
 @Component({
   selector: 'app-card',
@@ -9,24 +8,9 @@ import { MemberCard } from 'src/app/shared/model/member-card.model';
 })
 export class CardComponent implements OnInit {
   @Input()
-  member: MemberCard;
-  imgSrcSet: string;
+  member: Card;
 
-  srcSetImages: string;
   constructor() {}
 
-  ngOnInit(): void {
-    this.srcSetImages = this.generateSrcSet(this.member.images);
-  }
-
-  generateSrcSet(images: Image) {
-    let string = '';
-    Object.entries(images).map((image) => {
-      const size = image[0].substring(1) + 'w';
-      const url = image[1];
-      string += `${url} ${size} ,`;
-    });
-
-    return string;
-  }
+  ngOnInit(): void {}
 }
